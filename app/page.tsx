25dc83d0df3fd6f43d337e3b8afd0ab41934b7b2
vaibhav chauhan <vaibhavc.hestabit@gmail.com>
2025-12-01 13:52:07 +0530

    initial Next.js project including core configurations, UI components, and assets.

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>Stock Market</h1>
+        <Button className="">Get Started</Button>
+    </div>
+  )
+}
+
+export default Home 
